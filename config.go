caf289004ab1711d677d821e16afe537325125a1
Vicente Olmedo <vicente@storacha.network>
2025-07-21 19:26:57 +0200

    add hot staging configuration for Go tests

--- config.go
+++ config.go
@@ -4,16 +4,28 @@ import (
 	"net/url"
 	"os"
 
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/storacha/go-ucanto/did"
 )
 
-var IndexingServicePrincipal = Must(did.Parse("did:web:indexer.storacha.network"))
-var IndexingServiceURL = Deref(Must(url.Parse("https://indexer.storacha.network")))
+var log = logging.Logger("network-tester")
+
+var IndexingServicePrincipal did.DID
+var IndexingServiceURL url.URL
 
 func init() {
-	if os.Getenv("NETWORK") == "staging-warm" {
-		IndexingServicePrincipal = Must(did.Parse("did:web:indexer.warm.storacha.network"))
-		IndexingServiceURL = Deref(Must(url.Parse("https://indexer.warm.storacha.network")))
+	switch os.Getenv("NETWORK") {
+	case "", "hot":
+		IndexingServicePrincipal = Must(did.Parse("did:web:indexer.storacha.network"))
+		IndexingServiceURL = Deref(Must(url.Parse("https://indexer.storacha.network")))
+	case "staging":
+		IndexingServicePrincipal = Must(did.Parse("did:web:staging.indexer.storacha.network"))
+		IndexingServiceURL = Deref(Must(url.Parse("https://staging.indexer.storacha.network")))
+	case "staging-warm":
+		IndexingServicePrincipal = Must(did.Parse("did:web:staging.indexer.warm.storacha.network"))
+		IndexingServiceURL = Deref(Must(url.Parse("https://staging.indexer.warm.storacha.network")))
+	default:
+		panic("unknown network: " + os.Getenv("NETWORK"))
 	}
 }
 
