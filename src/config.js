b8f6d7f60557b7ca8c61adebb01d692432b9ceea
Alan Shaw <alan@storacha.network>
2025-10-24 18:04:07 +0100

    feat: space/index/add with retrieval authorization

--- src/config.js
+++ src/config.js
@@ -7,9 +7,11 @@ import * as Ed25519 from '@storacha/client/principal/ed25519'
 import { SHARD_SIZE } from '@storacha/upload-client/sharding'
 import * as DID from '@ipld/dag-ucan/did'
 import Package from '../package.json' with { type: 'json' }
-import { gb } from './lib.js'
+import { gb, mb } from './lib.js'
 
 dotenv.config()
+
+export const network = process.env.NETWORK
 
 /** Geographic region where the test is being run. */
 export const region = process.env.REGION || 'unknown'
@@ -22,45 +24,60 @@ export const maxBytes = process.env.MAX_BYTES
 /** Maximum bytes for a single upload. */
 export const maxPerUploadBytes = process.env.MAX_PER_UPLOAD_BYTES
   ? parseInt(process.env.MAX_PER_UPLOAD_BYTES)
-  : 1 * gb
+  : 5 * mb
 
 /** Maximum CAR shard size. */
-export const maxShardSize = process.env.NETWORK === 'staging-warm'
+export const maxShardSize = network === 'staging-warm'
   ? 266_338_304 // https://gist.github.com/alanshaw/be76c3d4ff555c3a0ee9f5b6e96b5436
   : SHARD_SIZE
 
 const headers = { ...Service.defaultHeaders }
 headers['X-Client'] += ' UploadTester/' + Package.version.split('.')[0]
 
-const stagingWarmConnection = Service.uploadServiceConnection({
+const stagingWarmUploadConnection = Service.uploadServiceConnection({
   id: DID.parse('did:web:staging.up.warm.storacha.network'),
   url: new URL('https://staging.up.warm.storacha.network'),
   headers
 })
 
-const stagingConnection = Service.uploadServiceConnection({
+const stagingUploadConnection = Service.uploadServiceConnection({
   id: DID.parse('did:web:staging.up.storacha.network'),
   url: new URL('https://staging.up.storacha.network'),
   headers
 })
 
-const hotConnection = Service.uploadServiceConnection({
+const hotUploadConnection = Service.uploadServiceConnection({
   id: DID.parse('did:web:up.storacha.network'),
   url: new URL('https://up.storacha.network'),
   headers
 })
 
-export const connection = process.env.NETWORK === 'staging-warm'
-  ? stagingWarmConnection
-  : process.env.NETWORK === 'staging'
-    ? stagingConnection
-    : hotConnection
+export const uploadConnection = network === 'staging-warm'
+  ? stagingWarmUploadConnection
+  : network === 'staging'
+    ? stagingUploadConnection
+    : hotUploadConnection
+
+export const indexerServicePrincipal = network === 'staging-warm'
+  ? DID.parse('did:web:staging.indexer.warm.storacha.network')
+  : network === 'staging'
+    ? DID.parse('did:web:staging.indexer.storacha.network')
+    : DID.parse('did:web:indexer.storacha.network')
+
+export const indexerServiceURL = network === 'staging-warm'
+  ? new URL('https://staging.indexer.warm.storacha.network')
+  : network === 'staging'
+    ? new URL('https://staging.indexer.storacha.network')
+    : new URL('https://indexer.storacha.network')
 
 export const id = Ed25519.parse(process.env.PRIVATE_KEY ?? '')
 
 export const proof = await Proof.parse(process.env.PROOF ?? '')
 
-export const spaceDID = DID.parse(proof.capabilities[0].with).did()
+
+export const spaceDID =
+  /** @type {import('@storacha/client/types').SpaceDID} */
+  (DID.parse(proof.capabilities[0].with).did())
 
 export const dataDir = path.join(import.meta.dirname, '..', 'data')
 
